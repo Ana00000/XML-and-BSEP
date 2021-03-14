03795856c262fb9009f06a31d75a89a5e8941481
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2021-03-14 12:42:28 +0100

    BSEP-chore: Initialization SQL script

---
+++ BSEP/bsep/src/main/resources/data-postgres.sql
@@ -0,0 +1,11 @@
+INSERT INTO certificate(id, alias, end_date, is_expired, issuer_id, serial_number, signature_algorithm_id, start_date, subject_id, version) VALUES (nextval('certificate_id_seq'), 'certificate1', '2021-03-10', FALSE, 1000, '102233045', 'SHA-256', '2020-03-10', 10225, 'V1');
+
+INSERT INTO certificate(id, alias, end_date, is_expired, issuer_id, serial_number, signature_algorithm_id, start_date, subject_id, version) VALUES (nextval('certificate_id_seq'), 'certificate2', '2021-03-10', FALSE, 1000, '202233045', 'SHA-256', '2020-03-10', 10225, 'V2');
+
+INSERT INTO certificate(id, alias, end_date, is_expired, issuer_id, serial_number, signature_algorithm_id, start_date, subject_id, version) VALUES (nextval('certificate_id_seq'), 'certificate3', '2021-03-10', FALSE, 1000, '302233045', 'SHA-256', '2020-03-10', 10225, 'V3');
+
+INSERT INTO certificate(id, alias, end_date, is_expired, issuer_id, serial_number, signature_algorithm_id, start_date, subject_id, version) VALUES (nextval('certificate_id_seq'), 'certificate4', '2021-03-10', FALSE, 1000, '402233045', 'SHA-256', '2020-03-10', 10225, 'V4');
+
+INSERT INTO certificate(id, alias, end_date, is_expired, issuer_id, serial_number, signature_algorithm_id, start_date, subject_id, version) VALUES (nextval('certificate_id_seq'), 'certificate5', '2021-03-10', FALSE, 1000, '502233045', 'SHA-256', '2020-03-10', 10225, 'V5');
+
+INSERT INTO certificate(id, alias, end_date, is_expired, issuer_id, serial_number, signature_algorithm_id, start_date, subject_id, version) VALUES (nextval('certificate_id_seq'), 'certificate6', '2021-03-10', FALSE, 1000, '602233045', 'SHA-256', '2020-03-10', 10225, 'V6');
