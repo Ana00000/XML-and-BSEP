a3c223b97f333154929191518046e87f4ba2fcfe
trki293 <59773039+trki293@users.noreply.github.com>
2021-04-18 12:17:30 +0200

    BSEP-fix: Fixing certificate problems

--- BSEP/bsep/src/main/frontend/vue.config.js
+++ BSEP/bsep/src/main/frontend/vue.config.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 module.exports = {
-  devServer: {
-    host: 'localhost',
-    disableHostCheck: true,
-    https: true,
+  devServer:{
+    https:true,
     key: fs.readFileSync('../../../localhost.pem'),
     cert: fs.readFileSync('../../../localhost.cer'),
-    public: 'https://localhost:8080/'
+    ca: fs.readFileSync('../../../localhost.pem')
   },
-
   transpileDependencies: [
     'vuetify'
   ]
