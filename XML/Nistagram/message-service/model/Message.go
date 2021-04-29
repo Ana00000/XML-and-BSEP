59744e93c3913376111e0dbd71c18ee4ea299d26
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2021-04-29 14:55:09 +0200

    XML-feat: Added Message structure

---
+++ XML/Nistagram/message-service/model/Message.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Message struct {
+	ID uuid.UUID `json: "id"`
+	ContentID uuid.UUID `json:"contentID"`
+	Content MessageContent
+	IsDisposable bool `json:"isDisposable" gorm:"not null"`
+	CreationDate time.Time `json:"creationDate" gorm:"not null"`
+	SenderUserID string `json:"senderUserID" gorm:"not null"`
+	ReceiverUserID string `json:"receiverUserID" gorm:"not null"`
+	IsDeleted bool `json:"isDeleted" gorm:"not null"`
+}
+
+
