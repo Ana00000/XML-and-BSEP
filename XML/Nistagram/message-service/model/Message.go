02342170e84c9e04062f75fb40f6478de9b6087b
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2021-04-30 16:09:56 +0200

    XML-refactor: Changed type in Message and MessageDTO structures
    string -> uuid.UUID

--- XML/Nistagram/message-service/model/Message.go
+++ XML/Nistagram/message-service/model/Message.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Message struct {
-	ID uuid.UUID `json: "id"`
+	ID uuid.UUID `json:"id"`
 	MessageContentID uuid.UUID `json:"messageContentID"`
 	IsDisposable bool `json:"isDisposable" gorm:"not null"`
 	CreationDate time.Time `json:"creationDate" gorm:"not null"`
-	SenderUserID string `json:"senderUserID" gorm:"not null"`
-	ReceiverUserID string `json:"receiverUserID" gorm:"not null"`
+	SenderUserID uuid.UUID `json:"senderUserID" gorm:"not null"`
+	ReceiverUserID uuid.UUID `json:"receiverUserID" gorm:"not null"`
 	IsDeleted bool `json:"isDeleted" gorm:"not null"`
 }
 
--- XML/Nistagram/message-service/model/MessageContent.go
+++ XML/Nistagram/message-service/model/MessageContent.go
@@ -3,6 +3,6 @@ package model
 import "github.com/google/uuid"
 
 type MessageContent struct{
-	ID uuid.UUID `json: "id"`
+	ID uuid.UUID `json:"id"`
 	Text string `json:"text" gorm:"not null"`
 }
