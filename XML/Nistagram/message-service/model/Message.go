530317dc034dc9e8536491e36470b9a124101830
trki293 <59773039+trki293@users.noreply.github.com>
2021-05-03 15:41:41 +0200

    XML-fix : Fixing dependecy between message and message substance

--- XML/Nistagram/message-service/model/Message.go
+++ XML/Nistagram/message-service/model/Message.go
@@ -8,7 +8,7 @@ import (
 
 type Message struct {
 	ID uuid.UUID `json:"id"`
-	MessageSubstanceID uuid.UUID `json:"messageContentID" gorm:"not null"`
+	MessageSubstance MessageSubstance `json:"message_substance" gorm:"foreignKey:MessageId"`
 	IsDisposable bool `json:"isDisposable" gorm:"not null"`
 	CreationDate time.Time `json:"creationDate" gorm:"not null"`
 	SenderUserID uuid.UUID `json:"senderUserID" gorm:"not null"`
--- XML/Nistagram/message-service/model/MessageSubstance.go
+++ XML/Nistagram/message-service/model/MessageSubstance.go
@@ -9,5 +9,6 @@ import (
 type MessageSubstance struct{
 	ID uuid.UUID `json:"id"`
 	Contents []messageContentPath.MessageContent `json:"contents" gorm:"foreignKey:MessageSubstanceId"`
+	MessageId uuid.UUID `json:"message_id" gorm:"not null"`
 	Text string `json:"text" gorm:"not null"`
 }
