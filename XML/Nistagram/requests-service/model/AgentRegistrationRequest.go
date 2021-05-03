4fd1cb616463301b7580545a9ab3cdfc13d0a26a
trki293 <59773039+trki293@users.noreply.github.com>
2021-05-03 16:10:54 +0200

    XML-fix : Fixing dependecy between registered user and inappropriate request

--- XML/Nistagram/requests-service/model/AgentRegistrationRequest.go
+++ XML/Nistagram/requests-service/model/AgentRegistrationRequest.go
@@ -1,13 +1,14 @@
 package model
 
 import (
+	agentPath "../../user-service/model"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type AgentRegistrationRequest struct {
-	ID uuid.UUID `json: "id"`
-	UserId uuid.UUID `json:"userId" gorm:"not null"`
+	ID uuid.UUID `json:"id"`
+	Agent agentPath.Agent `json:"agent" gorm:"foreignKey:AgentRegistrationRequestId"`
 }
 
 func(agentRegistrationRequest * AgentRegistrationRequest) BeforeCreate(scope *gorm.DB) error {
