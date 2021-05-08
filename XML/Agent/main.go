e14d58daef4358868dde88e256c7a193c9bb6266
velicko-tam <tamara.velickovic158@uns.ac.rs>
2021-05-08 14:59:40 +0200

    XML-feat: Agent app main

---
+++ XML/Agent/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"./handler"
+	"./model"
+	"./repository"
+	"./service"
+	"fmt"
+	_ "fmt"
+	_ "github.com/antchfx/xpath"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+	_ "os"
+	_ "strconv"
+)
+
+func initDB() *gorm.DB{
+	dsn := "host=localhost user=postgres password=root dbname=nistagram-db port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&model.AgentUser{}, &model.Product{})
+	return db
+}
+
+func initAgentUserRepo(database *gorm.DB) *repository.AgentUserRepository{
+	return &repository.AgentUserRepository { Database: database }
+}
+
+func initAgentUserServices(repo *repository.AgentUserRepository) *service.AgentUserService{
+	return &service.AgentUserService { Repo: repo }
+}
+
+func initAgentUserHandler(service *service.AgentUserService) *handler.AgentUserHandler{
+	return &handler.AgentUserHandler { Service: service }
+}
+
+func initProductRepo(database *gorm.DB) *repository.ProductRepository{
+	return &repository.ProductRepository { Database: database }
+}
+
+func initProductServices(repo *repository.ProductRepository) *service.ProductService{
+	return &service.ProductService { Repo: repo }
+}
+
+func initProductHandler(service *service.ProductService) *handler.ProductHandler{
+	return &handler.ProductHandler { Service: service }
+}
+
+
+func handleFunc(handlerAgentUser *handler.AgentUserHandler, handlerProduct *handler.ProductHandler){
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/agent_user/", handlerAgentUser.CreateAgentUser).Methods("POST")
+	router.HandleFunc("/product/", handlerProduct.CreateProduct).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", "8087"), router))
+}
+
+func main() {
+	database := initDB()
+
+	repoAgentUser:= initAgentUserRepo(database)
+	serviceAgentUser := initAgentUserServices(repoAgentUser)
+	handlerAgentUser := initAgentUserHandler(serviceAgentUser)
+
+	repoProduct := initProductRepo(database)
+	serviceProduct := initProductServices(repoProduct)
+	handlerProduct := initProductHandler(serviceProduct)
+
+	handleFunc(handlerAgentUser, handlerProduct)
+}
