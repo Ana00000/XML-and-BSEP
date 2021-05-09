3429f5bda915d8e5616398043f292d9ddacfd7ff
Ana Atanacković <anaatanackovic98@gmail.com>
2021-05-09 18:19:58 +0200

    XML-feat: frontend setup, registration and login parts for front

---
+++ XML/Nistagram-frontend/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/logIn',
+    name: 'LogIn',
+    component: () => import('../views/LogIn.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/confirmRegistration/:id',
+    name: 'ConfirmRegistration',
+    component: () => import('../views/ConfirmRegistration.vue')
+  },
+  {
+    path: '/recoverPasswordEmail',
+    name: 'RecoverPasswordEmail',
+    component: () => import('../views/RecoverPasswordEmail.vue')
+  },
+  {
+    path: '/changePasswordByToken/:id',
+    name: 'ChangePasswordByToken',
+    component: () => import('../views/ChangePasswordByToken.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ XML/Nistagram-frontend/src/views/LogIn.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-card style="margin-top: 10%" width="20%" class="mx-auto">
+    <v-card-title class="justify-center">
+      <h1 class="display-1 mt-5">Login</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form class="mx-auto ml-5 mr-5">
+        <v-text-field
+          label="Username"
+          v-model="username"
+          prepend-icon="mdi-account-circle"
+        /> 
+        <v-text-field
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          v-model="password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+        />
+        <a class="center" href="/recoverPasswordEmail">You forget your password?</a>
+      </v-form>
+      
+    </v-card-text>
+    <div >
+        
+    </div>
+    <v-card-actions>
+      <v-btn color="primary" class="mx-auto mb-5" large v-on:click="logIn">
+        Log in
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import axios from "axios";
+function redirectLoggedUser() {
+  var tokenString = "";
+  tokenString = localStorage.getItem("token");
+  const config = {
+    headers: { Authorization: `Bearer ${tokenString}` },
+  };
+  axios.get("https://localhost:8080/users/redirectMeToMyHomePage", config).then(
+    (response) => {
+      console.log(response);
+      window.location.href = response.data;
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+
+export default {
+  name: "Login",
+  data: () => ({
+    showPassword: false,
+    username: "",
+    password: "",
+    users: [],
+  }),
+  computed: {
+    user() {
+      return { username: this.username, password: this.password };
+    },
+  },
+  methods: {
+    logIn() {
+      this.$http
+        .post("https://localhost:8080/users/login", this.user)
+        .then((resp) => {
+          console.log(resp.data);
+          localStorage.setItem("token", resp.data.accessToken);
+          localStorage.setItem("username", this.user.username);
+          localStorage.setItem("role", resp.data.role);
+          redirectLoggedUser();
+        })
+        .catch((er) => {
+          alert("Invalid username and/or password! Please, try again!");
+          this.username = '';
+          this.password = '';
+          console.log(er.response.data);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.helloMessage {
+  font-weight: bolder;
+  font-size: 20px;
+  height: 50px;
+}
+
+.center {
+  padding: 31px;
+  text-align: center;
+}
+
+#certificateCard {
+  margin-top: 5%;
+  width: 70%;
+  height: 760px;
+  overflow-y: scroll;
+}
+</style>
