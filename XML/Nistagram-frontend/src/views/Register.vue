699d83918ef99befa9654bddbbcbac062c816bdc
Ana Atanacković <anaatanackovic98@gmail.com>
2021-05-12 17:33:41 +0200

    XML-feat: menu bar view based on user types

--- XML/Nistagram-frontend/src/views/Register.vue
+++ XML/Nistagram-frontend/src/views/Register.vue
@@ -289,7 +289,7 @@ export default {
       var dOBSYear = dateOfBirthSplit[0];
       var dOBSMonth = dateOfBirthSplit[1];
       var dOBSDay = dateOfBirthSplit[2];
-      if (dOBSYear > 3000 || dOBSYear < 1900){
+      if (dOBSYear > 2021 || dOBSYear < 1900){
         alert("Year of date of birth isn't valid");
         return false;
       } else if (dOBSMonth > 12 || dOBSMonth < 0){
