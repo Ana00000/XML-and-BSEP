1e7c46c9dce8196d49b96040f9a3d3d160ba117a
velicko-tam <tamara.velickovic158@uns.ac.rs>
2021-05-17 13:24:13 +0200

    XML-feat: Follow request model setup

---
+++ XML/Nistagram/requests-service/model/FollowRequest.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type FollowRequest struct {
+	ID uuid.UUID `json:"id"`
+	ClassicUserId uuid.UUID `json:"classic_user_id" gorm:"not null"`
+	FollowerUserId uuid.UUID `json:"follower_user_id" gorm:"not null"`
+	FollowRequestStatus FollowRequestStatus `json:"follow_request_status" gorm:"not null"`
+}
+
+func(followRequest * FollowRequest) BeforeCreate(scope *gorm.DB) error {
+	followRequest.ID = uuid.New()
+	return nil
+}
