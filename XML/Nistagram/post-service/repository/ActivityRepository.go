15e785415f8de132bc93c5d54befc2074e672148
velicko-tam <tamara.velickovic158@uns.ac.rs>
2021-05-20 14:57:57 +0200

    XML-feat: Find all likes, dislikes and favorites repository logic

--- XML/Nistagram/post-service/repository/ActivityRepository.go
+++ XML/Nistagram/post-service/repository/ActivityRepository.go
@@ -1,8 +1,9 @@
 package repository
 
 import (
-	"github.com/xml/XML-and-BSEP/XML/Nistagram/post-service/model"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/xml/XML-and-BSEP/XML/Nistagram/post-service/model"
 	"gorm.io/gorm"
 )
 
@@ -14,4 +15,25 @@ func (repo * ActivityRepository) CreateActivity(activity *model.Activity) error 
 	result := repo.Database.Create(activity)
 	fmt.Print(result)
 	return nil
+}
+
+func (repo * ActivityRepository) FindAllLikesForPost(postId uuid.UUID) []model.Activity{
+	var likes []model.Activity
+
+	repo.Database.Select("*").Where("post_id = ? and liked_status = ?", postId, 0).Find(&likes)
+	return likes
+}
+
+func (repo * ActivityRepository) FindAllDislikesForPost(postId uuid.UUID) []model.Activity{
+	var dislikes []model.Activity
+
+	repo.Database.Select("*").Where("post_id = ? and liked_status = ?", postId, 1).Find(&dislikes)
+	return dislikes
+}
+
+func (repo * ActivityRepository) FindAllFavoritesForPost(postId uuid.UUID) []model.Activity{
+	var favorites []model.Activity
+
+	repo.Database.Select("*").Where("post_id = ? and is_favorite = ?", postId, true).Find(&favorites)
+	return favorites
 }
