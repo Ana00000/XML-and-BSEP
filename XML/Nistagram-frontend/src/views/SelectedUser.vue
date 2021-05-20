b9a580f87bb9e9d6f88726d178de7cbaa7970967
Ana Atanacković <anaatanackovic98@gmail.com>
2021-05-20 16:20:41 +0200

    XML-feat: get all stories for selected profile from registered and not registered users

--- XML/Nistagram-frontend/src/views/SelectedUser.vue
+++ XML/Nistagram-frontend/src/views/SelectedUser.vue
@@ -89,6 +89,61 @@
     <v-container grid-list-lg >
       <div class="spacingOne" />
       <div class="title">
+        <h1>Stories</h1>
+      </div>
+      <div class="spacingTwo" />
+      <v-layout row>
+        <v-flex lg4 v-for="item in stories" :key="item.id" class="space-bottom">
+          <v-card class="mx-auto">
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-subtitle>{{
+                  item.description
+                }}</v-list-item-subtitle>
+                <v-list-item-title>{{ item.tags }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item three-line v-if="item.type == 'VIDEO'">
+              <v-list-item-content> 
+               <video width="320" height="240" controls>
+                  <source :src="require(`../../../Media/${ item.path }`)" type="video/mp4">
+                </video>
+              </v-list-item-content>
+            </v-list-item>
+
+             <v-list-item three-line v-if="item.type != 'VIDEO'">
+              <v-list-item-content> 
+                <img :src="require(`../../../Media/${ item.path }`)" alt class="icon" width="320" height="240"/>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-subtitle
+                  v-text="
+                    item.country +
+                    ' ' +
+                    item.city +
+                    ' ' +
+                    item.street_name +
+                    ' ' +
+                    item.street_number
+                  "
+                />
+                <v-list-item-subtitle>{{
+                  item.creation_date
+                }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container grid-list-lg >
+      <div class="spacingOne" />
+      <div class="title">
         <h1>Posts</h1>
       </div>
       <div class="spacingTwo" />
@@ -140,6 +195,7 @@
         </v-flex>
       </v-layout>
     </v-container>
+
   </div>
 </template>
 
@@ -156,6 +212,7 @@ export default {
     isHiddenSendFollowRequest: true,
     isHiddenFollowRequestSent: true,
     posts: [],
+    stories: [],
   }),
   mounted() {
     this.selectedUser = localStorage.getItem("selectedUserId");
@@ -229,6 +286,13 @@ export default {
         .get("http://localhost:8084/find_all_posts_for_reg?id=" + this.selectedUser + "&logId=" + localStorage.getItem("userId"))
         .then((response) => {
           this.posts = response.data;
+        })
+        .catch(console.log);
+
+        this.$http
+        .get("http://localhost:8086/find_all_stories_for_reg?id=" + this.selectedUser + "&logId=" + localStorage.getItem("userId"))
+        .then((response) => {
+          this.stories = response.data;
         })
         .catch(console.log);
     },
