14eb4f94402aea7aefa8c6372a9eebbf4f068f5e
velicko-tam <tamara.velickovic158@uns.ac.rs>
2021-05-20 11:15:14 +0200

    XML-feat: Find all stories methods

--- XML/Nistagram/story-service/repository/SingleStoryRepository.go
+++ XML/Nistagram/story-service/repository/SingleStoryRepository.go
@@ -1,8 +1,10 @@
 package repository
 
 import (
-	"github.com/xml/XML-and-BSEP/XML/Nistagram/story-service/model"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/xml/XML-and-BSEP/XML/Nistagram/story-service/model"
+	userModel "github.com/xml/XML-and-BSEP/XML/Nistagram/user-service/model"
 	"gorm.io/gorm"
 )
 
@@ -14,4 +16,56 @@ func (repo * SingleStoryRepository) CreateSingleStory(singleStory *model.SingleS
 	result := repo.Database.Create(singleStory)
 	fmt.Print(result)
 	return nil
+}
+
+func (repo *SingleStoryRepository) FindAllStories() []model.Story {
+	var stories []model.Story
+	repo.Database.Select("*").Find(&stories)
+	return stories
+}
+
+func (repo *SingleStoryRepository) FindByID(ID uuid.UUID) *model.Story {
+	story := &model.Story{}
+	if repo.Database.First(&story, "id = ? and is_deleted = ?", ID, false).RowsAffected == 0 {
+		return nil
+	}
+	return story
+}
+
+// USED WHEN CLICKING ON A SELECTED USER (YOU CAN SELECT FROM A LIST OF ONLY VALID USERS)
+func (repo *SingleStoryRepository) FindAllStoriesForUser(userId uuid.UUID) []model.Story {
+	var stories []model.Story
+	repo.Database.Select("*").Where("user_id = ? and is_deleted = ?", userId, false).Find(&stories)
+	return stories
+}
+
+
+// FIND ALL NOT DELETED VALID STORIES THAT LOGGED IN USER FOLLOWS
+func (repo *SingleStoryRepository) FindAllFollowingStories(followings []userModel.ClassicUserFollowings) []model.Story {
+	var allStories = repo.FindAllStories()
+	var allFollowingStories []model.Story
+
+	for i:= 0; i< len(allStories); i++{
+		for j := 0; j < len(followings); i++{
+			if (allStories[i].UserId == followings[i].FollowingUserId) && (allStories[i].IsDeleted == false){
+				allFollowingStories = append(allFollowingStories, allStories[i])
+			}
+		}
+	}
+	return allFollowingStories
+}
+
+func (repo *SingleStoryRepository) FindAllPublicStoriesNotRegisteredUser(allValidUsers []userModel.ClassicUser) []model.Story {
+	var allStories = repo.FindAllStories()
+	var allPublicStories []model.Story
+
+	for i:=0;i<len(allStories);i++{
+		for j:=0; j<len(allValidUsers);j++{
+			if allStories[i].UserId == allValidUsers[j].ID {
+				allPublicStories = append(allPublicStories, allStories[i])
+			}
+		}
+	}
+
+	return allPublicStories
 }
