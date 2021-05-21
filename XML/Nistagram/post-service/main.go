055f049df5732a1c420a5500f5f4aa31c4854724
velicko-tam <tamara.velickovic158@uns.ac.rs>
2021-05-21 20:40:03 +0200

    XML-feat: Search all tags for not registered user frontend

--- XML/Nistagram/post-service/main.go
+++ XML/Nistagram/post-service/main.go
@@ -228,16 +228,16 @@ func handleFunc(handlerActivity *handler.ActivityHandler, handlerComment *handle
 	router.HandleFunc("/find_selected_post_for_logged_user", handlerSinglePost.FindSelectedPostByIdForLoggedUser).Methods("GET")
 
 	//SEARCH FOR NOT REGISTERED USER
-	router.HandleFunc("/find_all_tags_for_public_posts", handlerSinglePost.FindAllTagsForPublicPosts).Methods("GET")
-	router.HandleFunc("/find_all_locations_for_public_posts", handlerSinglePost.FindAllLocationsForPublicPosts).Methods("GET")
+	router.HandleFunc("/find_all_tags_for_public_posts/", handlerSinglePost.FindAllTagsForPublicPosts).Methods("GET")
+	router.HandleFunc("/find_all_locations_for_public_posts/", handlerSinglePost.FindAllLocationsForPublicPosts).Methods("GET")
 
 	//metoda koja se poziva kada neregistrovani user pretrazi tag pa klikne na njega - prikazuju se svi PUBLIC, NOT DELETED postovi sa tim tagom
 	router.HandleFunc("/find_all_posts_for_tag", handlerSinglePost.FindAllPostsForTag).Methods("GET")
 	router.HandleFunc("/find_all_posts_for_location", handlerSinglePost.FindAllPostsForLocation).Methods("GET")
 
 	//SEARCH FOR REGISTERED USER
-	router.HandleFunc("/find_all_tags_for_public_and_friends_posts", handlerSinglePost.FindAllTagsForPublicAndFollowingPosts).Methods("GET")
-	router.HandleFunc("/find_all_locations_for_public_friends_posts", handlerSinglePost.FindAllLocationsForPublicAndFollowingPosts).Methods("GET")
+	router.HandleFunc("/find_all_tags_for_public_and_friends_posts/", handlerSinglePost.FindAllTagsForPublicAndFollowingPosts).Methods("GET")
+	router.HandleFunc("/find_all_locations_for_public_friends_posts/", handlerSinglePost.FindAllLocationsForPublicAndFollowingPosts).Methods("GET")
 
 	//metoda koja se poziva kada neregistrovani user pretrazi tag pa klikne na njega - prikazuju se svi PUBLIC, NOT DELETED postovi sa tim tagom
 	router.HandleFunc("/find_all_posts_for_tag_reg_user", handlerSinglePost.FindAllPostsForTagRegUser).Methods("GET")
