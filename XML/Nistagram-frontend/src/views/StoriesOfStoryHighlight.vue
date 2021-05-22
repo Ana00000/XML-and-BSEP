95d31cfeedd58a7db5fb6310b879d4e4781ea185
Ana Atanacković <anaatanackovic98@gmail.com>
2021-05-22 18:47:20 +0200

    XML-feat: get story on click from stories in highlights

--- XML/Nistagram-frontend/src/views/StoriesOfStoryHighlight.vue
+++ XML/Nistagram-frontend/src/views/StoriesOfStoryHighlight.vue
@@ -107,6 +107,12 @@ export default {
         })
         .catch(console.log);
     },
+    getMyStory(item){
+      localStorage.setItem("mySelectedUserId", item.user_id);
+      localStorage.setItem("mySelectedStoryId", item.story_id);
+
+      window.location.href = "http://localhost:8081/storyByIdWithoutActivity";
+    }
   },
 };
 </script>
