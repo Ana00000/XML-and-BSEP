6886c7d28c670ee949e9db0400ebb1b0eccee113
trki293 <59773039+trki293@users.noreply.github.com>
2021-06-09 18:46:15 +0200

    BSEP-feat: Tag microservices logging mehchanism implemented

--- XML/Nistagram/tag-service/handler/PostAlbumTagPostAlbumsHandler.go
+++ XML/Nistagram/tag-service/handler/PostAlbumTagPostAlbumsHandler.go
@@ -2,23 +2,33 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/tag-service/dto"
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/tag-service/model"
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/tag-service/service"
 	"net/http"
 	_ "strconv"
+	"time"
 )
 
 type PostAlbumTagPostAlbumsHandler struct {
 	Service * service.PostAlbumTagPostAlbumsService
+	LogInfo *logrus.Logger
+	LogError *logrus.Logger
 }
 
+//CRPOALBTGPOALBMS9832
 func (handler *PostAlbumTagPostAlbumsHandler) CreatePostAlbumTagPostAlbums(w http.ResponseWriter, r *http.Request) {
 	var postAlbumTagPostAlbumsDTO dto.PostAlbumTagPostAlbumsDTO
 	err := json.NewDecoder(r.Body).Decode(&postAlbumTagPostAlbumsDTO)
 	if err != nil {
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "PostAlbumTagPostAlbumsHandler",
+			"action":   "CRPOALBTGPOALBMS9832",
+			"timestamp":   time.Now().String(),
+		}).Error("Wrong cast json to PostAlbumTagPostAlbumsDTO!")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -32,18 +42,37 @@ func (handler *PostAlbumTagPostAlbumsHandler) CreatePostAlbumTagPostAlbums(w htt
 
 	err = handler.Service.CreatePostAlbumTagPostAlbums(&postAlbumTagPostAlbums)
 	if err != nil {
-		fmt.Println(err)
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "PostAlbumTagPostAlbumsHandler",
+			"action":   "CRPOALBTGPOALBMS9832",
+			"timestamp":   time.Now().String(),
+		}).Error("Failed adding post album tag for post album!")
 		w.WriteHeader(http.StatusExpectationFailed)
 	}
+
+	handler.LogInfo.WithFields(logrus.Fields{
+		"status": "success",
+		"location":   "PostAlbumTagPostAlbumsHandler",
+		"action":   "CRPOALBTGPOALBMS9832",
+		"timestamp":   time.Now().String(),
+	}).Info("Successfully added post album tag for post album!")
 
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 }
 
+//FIDALTGSFRPOALBTGPSTALBMS321
 func (handler *PostAlbumTagPostAlbumsHandler) FindAllTagsForPostAlbumTagPostAlbums(w http.ResponseWriter, r *http.Request) {
 	var postAlbumFullDTO []dto.PostAlbumFullDTO
 	err := json.NewDecoder(r.Body).Decode(&postAlbumFullDTO)
 	if err != nil {
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "PostAlbumTagPostAlbumsHandler",
+			"action":   "FIDALTGSFRPOALBTGPSTALBMS321",
+			"timestamp":   time.Now().String(),
+		}).Error("Wrong cast json to PostAlbumFullDTO!")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -52,15 +81,28 @@ func (handler *PostAlbumTagPostAlbumsHandler) FindAllTagsForPostAlbumTagPostAlbu
 
 	tagsForPostsJson, _ := json.Marshal(tags)
 	w.Write(tagsForPostsJson)
+	handler.LogInfo.WithFields(logrus.Fields{
+		"status": "success",
+		"location":   "PostAlbumTagPostAlbumsHandler",
+		"action":   "FIDALTGSFRPOALBTGPSTALBMS321",
+		"timestamp":   time.Now().String(),
+	}).Info("Successfully founded tags for album tag for post album!")
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 }
 
+//FIDALTGSFRPOALB9231
 func (handler *PostAlbumTagPostAlbumsHandler) FindAllTagsForPostAlbum(w http.ResponseWriter, r *http.Request) {
 	var postAlbumFullDTO dto.PostAlbumFullDTO
 	err := json.NewDecoder(r.Body).Decode(&postAlbumFullDTO)
 	if err != nil {
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "PostAlbumTagPostAlbumsHandler",
+			"action":   "FIDALTGSFRPOALB9231",
+			"timestamp":   time.Now().String(),
+		}).Error("Wrong cast json to PostAlbumFullDTO!")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -69,6 +111,13 @@ func (handler *PostAlbumTagPostAlbumsHandler) FindAllTagsForPostAlbum(w http.Res
 
 	tagsForPostsJson, _ := json.Marshal(tags)
 	w.Write(tagsForPostsJson)
+
+	handler.LogInfo.WithFields(logrus.Fields{
+		"status": "success",
+		"location":   "PostAlbumTagPostAlbumsHandler",
+		"action":   "FIDALTGSFRPOALB9231",
+		"timestamp":   time.Now().String(),
+	}).Info("Successfully founded tags for post album!")
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
--- XML/Nistagram/tag-service/handler/TagHandler.go
+++ XML/Nistagram/tag-service/handler/TagHandler.go
@@ -2,50 +2,70 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/tag-service/dto"
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/tag-service/model"
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/tag-service/service"
 	"gopkg.in/go-playground/validator.v9"
 	"net/http"
 	_ "strconv"
+	"time"
 )
 
 type TagHandler struct {
 	Service * service.TagService
 	Validator *validator.Validate
+	LogInfo *logrus.Logger
+	LogError *logrus.Logger
 }
 
 type ReturnValueString struct {
 	ReturnValue string `json:"return_value"`
 }
-
+//FIDTGNMBYID0921
 func (handler *TagHandler) FindTagNameById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	fmt.Println("Dobijeni ID : "+id)
+	//fmt.Println("Dobijeni ID : "+id)
 	var tagName = handler.Service.FindTagNameById(uuid.MustParse(id))
-	fmt.Println("Dobijeni name : "+tagName)
+	//fmt.Println("Dobijeni name : "+tagName)
 	returnValue := ReturnValueString{ReturnValue: tagName}
 
 	returnValueJson, _ := json.Marshal(returnValue)
 	w.Write(returnValueJson)
-
+	handler.LogInfo.WithFields(logrus.Fields{
+		"status": "success",
+		"location":   "TagHandler",
+		"action":   "FIDTGNMBYID0921",
+		"timestamp":   time.Now().String(),
+	}).Info("Successfully founded tag name by id!")
 
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 }
-
+//CRTG7821
 func (handler *TagHandler) CreateTag(w http.ResponseWriter, r *http.Request) {
 	var tagDTO dto.TagDTO
 	if err := json.NewDecoder(r.Body).Decode(&tagDTO); err != nil {
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "TagHandler",
+			"action":   "CRTG7821",
+			"timestamp":   time.Now().String(),
+		}).Error("Wrong cast json to TagDTO!")
 		w.WriteHeader(http.StatusBadRequest) // 400
 		return
 	}
 
 	if err := handler.Validator.Struct(&tagDTO); err != nil {
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "TagHandler",
+			"action":   "CRTG7821",
+			"timestamp":   time.Now().String(),
+		}).Error("TagDTO fields doesn't in valid format!")
 		w.WriteHeader(http.StatusBadRequest) //400
 		return
 	}
@@ -61,6 +81,12 @@ func (handler *TagHandler) CreateTag(w http.ResponseWriter, r *http.Request) {
 
 	if findTag != nil && findTag.TagType==tagType{
 		tagJson, _ := json.Marshal(findTag.ID)
+		handler.LogInfo.WithFields(logrus.Fields{
+			"status": "success",
+			"location":   "TagHandler",
+			"action":   "CRTG7821",
+			"timestamp":   time.Now().String(),
+		}).Info("Successfully founded tag with same name!")
 		w.Write(tagJson)
 		w.WriteHeader(http.StatusAccepted)
 		w.Header().Set("Content-Type", "application/json")// 202
@@ -82,7 +108,12 @@ func (handler *TagHandler) CreateTag(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if err := handler.Service.CreateTag(&tag); err != nil {
-			fmt.Println(err)
+			handler.LogError.WithFields(logrus.Fields{
+				"status": "failure",
+				"location":   "TagHandler",
+				"action":   "CRTG7821",
+				"timestamp":   time.Now().String(),
+			}).Error("Failed creating tag!")
 			w.WriteHeader(http.StatusExpectationFailed) // 417
 			return
 		}
@@ -90,24 +121,43 @@ func (handler *TagHandler) CreateTag(w http.ResponseWriter, r *http.Request) {
 
 	tagIDJson, _ := json.Marshal(tag.ID)
 	w.Write(tagIDJson)
+	handler.LogInfo.WithFields(logrus.Fields{
+		"status": "success",
+		"location":   "TagHandler",
+		"action":   "CRTG7821",
+		"timestamp":   time.Now().String(),
+	}).Info("Successfully created tag!")
 
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 }
-
+//FIDTGFORID9180
 func (handler *TagHandler) FindTagForId(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 
 	tag := handler.Service.FindTagForId(uuid.MustParse(id))
 	tagJson, _ := json.Marshal(tag)
 	if tagJson != nil {
-		w.WriteHeader(http.StatusCreated)
+		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(tagJson)
+		handler.LogInfo.WithFields(logrus.Fields{
+			"status": "success",
+			"location":   "TagHandler",
+			"action":   "FIDTGFORID9180",
+			"timestamp":   time.Now().String(),
+		}).Info("Successfully founded tag for id!")
+		return
 	}
+	handler.LogError.WithFields(logrus.Fields{
+		"status": "failure",
+		"location":   "TagHandler",
+		"action":   "FIDTGFORID9180",
+		"timestamp":   time.Now().String(),
+	}).Error("Failed finding tag for id!")
 	w.WriteHeader(http.StatusBadRequest)
 }
-
+//FIDTGBYNM913
 func (handler *TagHandler) FindTagByName(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	name := vars["name"]
@@ -126,9 +176,22 @@ func (handler *TagHandler) FindTagByName(w http.ResponseWriter, r *http.Request)
 	}
 	tagJson, _ := json.Marshal(tagDTO)
 	if tagJson != nil {
-		w.WriteHeader(http.StatusCreated)
+		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(tagJson)
+		handler.LogInfo.WithFields(logrus.Fields{
+			"status": "success",
+			"location":   "TagHandler",
+			"action":   "FIDTGBYNM913",
+			"timestamp":   time.Now().String(),
+		}).Info("Successfully founded tag by name!")
+		return
 	}
+	handler.LogError.WithFields(logrus.Fields{
+		"status": "failure",
+		"location":   "TagHandler",
+		"action":   "FIDTGBYNM913",
+		"timestamp":   time.Now().String(),
+	}).Error("Failed finding tag by name!")
 	w.WriteHeader(http.StatusBadRequest)
 }
