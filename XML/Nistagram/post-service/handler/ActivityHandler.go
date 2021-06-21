bee5a8eb9beafad5c7b3e0140e7119f5d2190266
Ana Atanacković <anaatanackovic98@gmail.com>
2021-06-21 11:01:08 +0200

    XML-feat: activity notifications

--- XML/Nistagram/post-service/handler/ActivityHandler.go
+++ XML/Nistagram/post-service/handler/ActivityHandler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/post-service/dto"
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/post-service/model"
 	"github.com/xml/XML-and-BSEP/XML/Nistagram/post-service/service"
+	gomail "gopkg.in/mail.v2"
 	"net/http"
 	"os"
 	"time"
@@ -282,17 +283,6 @@ func (handler *ActivityHandler) UpdateActivity(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	/*if err := TokenValid(r); err != nil {
-		handler.LogError.WithFields(logrus.Fields{
-			"status": "failure",
-			"location":   "ActivityHandler",
-			"action":   "UPACT472",
-			"timestamp":   time.Now().String(),
-		}).Error("User doesn't logged in!")
-		w.WriteHeader(http.StatusUnauthorized) // 401
-		return
-	}*/
-
 	w.Header().Set("X-XSS-Protection", "1; mode=block")
 	var activityDTO dto.ActivityDTO
 
@@ -326,6 +316,108 @@ func (handler *ActivityHandler) UpdateActivity(w http.ResponseWriter, r *http.Re
 		"action":   "UPACT472",
 		"timestamp":   time.Now().String(),
 	}).Info("Successfully updated activity!")
+
+	//GET USERID FROM POSTID FOR ACTIVITYNOTIFICATION
+	var userId uuid.UUID
+	reqUrl := fmt.Sprintf("http://%s:%s/find_owner_of_post/%s", os.Getenv("POST_SERVICE_DOMAIN"), os.Getenv("POST_SERVICE_DOMAIN"), activityDTO.PostID)
+	err = getJson(reqUrl, &userId)
+	if err!=nil{
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "ActivityHandler",
+			"action":   "UPACT472",
+			"timestamp":   time.Now().String(),
+		}).Error("Failed to find owner of the post!")
+		w.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+
+	var user dto.ClassicUserDTO
+	reqUrlUser := fmt.Sprintf("http://%s:%s/get_user_by_id?id=%s", os.Getenv("USER_SERVICE_DOMAIN"), os.Getenv("USER_SERVICE_PORT"), userId)
+	err = getJson(reqUrlUser, &user)
+	if err!=nil{
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "ActivityHandler",
+			"action":   "UPACT472",
+			"timestamp":   time.Now().String(),
+		}).Error("Failed to find user by id!")
+		w.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+
+	//SEND EMAIL NOTIFICATION
+	handler.SendNotificationMail(user.Email, activityDTO)
+
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
+}
+
+func (handler *ActivityHandler) SendNotificationMail(email string, activity dto.ActivityDTO) {
+	m := gomail.NewMessage()
+
+	// Set E-Mail sender
+	m.SetHeader("From", "xml.ftn.uns@gmail.com")
+
+	// Set E-Mail receivers
+	m.SetHeader("To", email)
+
+	// Set E-Mail subject
+	m.SetHeader("Subject", "Confirmation mail")
+
+	var user dto.ClassicUserDTO
+	reqUrlUser := fmt.Sprintf("http://%s:%s/get_user_by_id?id=%s", os.Getenv("USER_SERVICE_DOMAIN"), os.Getenv("USER_SERVICE_PORT"), activity.UserID)
+	err := getJson(reqUrlUser, &user)
+	if err!=nil{
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "ActivityHandler",
+			"action":   "SEDCONFMAIL777",
+			"timestamp":   time.Now().String(),
+		}).Error("Failed to find user by id!")
+		panic(err)
+	}
+
+	// Set E-Mail body. You can set plain text or html with text/html
+	text := user.FirstName + " " + user.LastName + "\n\n\n reacted to your post!\n\nhttps://localhost:8081/postById/" + activity.PostID.String()
+	if activity.LikedStatus == 0 {
+		text += "\n\n\nPost is currently liked"
+	} else if activity.LikedStatus == 1 {
+		text += "\n\n\nPost is currently disliked"
+	}
+
+	if activity.IsFavorite == true {
+		text += " and favorited."
+	} else {
+		text += "."
+	}
+
+	text += "\n\n\nBest regards,\nTim25"
+	m.SetBody("text/plain", text)
+
+	// Settings for SMTP server
+	d := gomail.NewDialer("smtp.gmail.com", 587, "xml.ftn.uns@gmail.com", "XMLFTNUNS1")
+
+	// This is only needed when SSL/TLS certificate is not valid on server.
+	// In production this should be set to false.
+	//d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Now send E-Mail
+	if err := d.DialAndSend(m); err != nil {
+		//fmt.Println(err)
+		handler.LogError.WithFields(logrus.Fields{
+			"status": "failure",
+			"location":   "ActivityHandler",
+			"action":   "SEDCONFMAIL777",
+			"timestamp":   time.Now().String(),
+		}).Error("Failed sending email with confirmation token!")
+		panic(err)
+	}
+
+	handler.LogInfo.WithFields(logrus.Fields{
+		"status": "success",
+		"location":   "ActivityHandler",
+		"action":   "SEDCONFMAIL777",
+		"timestamp":   time.Now().String(),
+	}).Info("Successfully sent email with confirmation token!")
 }
